7bba1cb199fad1eb350101886cde910d17f39470
Luzie Freires <luzieann.freires@students.isatu.edu.ph>
2024-09-23 11:28:10 +0000

    first commit

---
+++ RWorksheet_Freires#2/RWorksheet_Freires#2.R
@@ -0,0 +1,16 @@
+1. Create a vector using : operator
+a) Sequence from -5 to 5. Write the R code and its output.
+Describe its output.
+
+```{r}
+num <- -5:5
+print(num)
+```
+- The output would be sequenced 
+
+b) What will be the value of x?
+  
+```{r}
+x <- 1:7
+print(X)
+```
